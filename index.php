db04aded0a984a8848079496ee50ca1deb323eb0
Guillermo Neugebauer <gn90at@gmail.com>
2013-11-10 14:50:33 +0100

    all commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Website</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+	
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
