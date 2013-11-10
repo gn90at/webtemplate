505fbe8f8f459a7acf8c8643397c2ca3061b45f1
Guillermo Neugebauer <gn90at@gmail.com>
2013-11-10 15:03:58 +0100

    update v0.1

--- index.php
+++ index.php
@@ -10,14 +10,17 @@
     <meta name="viewport" content="width=device-width">
 
 	<link rel="stylesheet" href="css/style.css">
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
 <body>
     <!--[if lt IE 7]>
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
 	
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.10.2.min.js"><\/script>')</script>
 	<script type="text/javascript" src="js/scripts.js"></script>
 </body>
 </html>
